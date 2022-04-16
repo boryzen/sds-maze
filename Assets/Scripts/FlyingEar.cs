93a1cdfffebef388f0851a48f18a91c5732b0099
vwesterlund <xwester@gmail.com>
2022-04-16 09:58:53 +0200

    Flying ear script
    FlyingEar is able to move ahead of the player

--- Assets/Scripts/FlyingEar.cs
+++ Assets/Scripts/FlyingEar.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FlyingEar : MonoBehaviour
 {
+
+    private float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,43 @@ public class FlyingEar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Alpha1 key was pressed.");
+
+            distance = 0;
+
+            transform.Translate(0, 0, distance);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Alpha2 key was pressed.");
+
+            distance = 5;
+
+            transform.Translate(0, 0, distance);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log("Alpha3 key was pressed.");
+
+            distance = 10;
+
+            transform.Translate(0, 0, distance);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("Alpha4 key was pressed.");
+
+            distance = 15;
+
+            transform.Translate(0, 0, distance);
+        }
+
     }
 }
