3dfbf60376e735099e1ff4ea992078a26700a27b
vwesterlund <xwester@gmail.com>
2022-05-09 23:50:55 +0200

    StopWatch script added

---
+++ Assets/Scripts/StopWatch.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StopWatch : MonoBehaviour
+{
+    bool timerActive = false;
+    float currentTime;
+    //public Text disvar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentTime = 0;
+
+        StartTimer();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (timerActive == true)
+        {
+            currentTime += Time.deltaTime;
+        }
+
+  //      TimeSpan time = TimeSpan.FromSeconds(currentTime);
+  //      disvar.text = time.ToString(@"mm\:ss\:fff");
+
+
+    }
+
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+    public void StopTimer()
+    {
+        timerActive &= false;   
+    }
+
+}
