aabdf2c6784d9b954a439833cee6d4a98b26dc75
vwesterlund <xwester@gmail.com>
2022-05-12 17:07:30 +0200

    Instruction for intro scene

--- Assets/Scripts/FlyingEar.cs
+++ Assets/Scripts/FlyingEar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 
@@ -15,35 +16,35 @@ public class FlyingEar : MonoBehaviour
     public Transform camera;
     // [Header("Key binds")]
     // //  public Input keyUp;
-    
+
     private float maxRadiusSq;
 
     // Start is called before the first frame update
 
     private bool canMoveFlag = true;
-    
+
     void Start()
     {
         // updateTransform(orientationTransform);
         // resetPosition();
-        maxRadiusSq = maxRadius*maxRadius;
+        maxRadiusSq = maxRadius * maxRadius;
         transform.parent = parent;
-        
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
 
-        
+
         float step = Time.deltaTime * earSpeed;
-        
+
         // Forward movement
         if (Input.GetKey(KeyCode.Alpha2) && canMoveFlag)
         {
             // Debug.Log("2 key was pressed.");
-            if(checkBounds())
+            if (checkBounds())
                 transform.Translate(Vector3.forward * step);
 
             // Back movement
@@ -52,7 +53,7 @@ public class FlyingEar : MonoBehaviour
         if (Input.GetKey(KeyCode.X) && canMoveFlag)
         {
             // Debug.Log("X key was pressed.");
-            if(checkBounds())
+            if (checkBounds())
                 transform.Translate(Vector3.back * step);
         }
 
@@ -64,12 +65,13 @@ public class FlyingEar : MonoBehaviour
             canMoveFlag = true;
             // Debug.Log("The Ear Reset");
         }
-        
-            // transform.rotation = cameraTransform.rotation;
+
+        // transform.rotation = cameraTransform.rotation;
         if (Input.GetKeyDown(KeyCode.F))
-        {   
-            if(transform.parent != null)
-            {   transform.parent = null;
+        {
+            if (transform.parent != null)
+            {
+                transform.parent = null;
                 canMoveFlag = false;
             }
             else
@@ -79,11 +81,22 @@ public class FlyingEar : MonoBehaviour
                 canMoveFlag = true;
             }
 
-        }
-        if(transform.parent == null)
-            transform.rotation = parent.rotation;
 
-    }  
+            if (transform.parent == null)
+            {
+                transform.rotation = parent.rotation;
+            }
+        }
+
+        if (!Input.GetKeyDown(KeyCode.N))
+        {
+            return;
+        }
+        Scene2();
+
+
+
+    }
     // Resets theposition of the ear to the position of the camera
     void resetPosition()
     {
@@ -101,5 +114,10 @@ public class FlyingEar : MonoBehaviour
             Debug.Log("Ear out of bounds");
             return false;
         }
+    }
+
+    private static void Scene2()
+    {
+        SceneManager.LoadScene("The labirinth");
     }
 }
--- Assets/Scripts/InstructionsIntro.cs
+++ Assets/Scripts/InstructionsIntro.cs
@@ -3,41 +3,122 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class InstructionsIntro : MonoBehaviour
 {
 
-    private int goalCounter = 0;
 
-    public Text disvar;
 
-    private int myInt;
+    public Text disvar = null;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        disvar.text = ("Use your mouse to move head\n" +
-            "U to move forward\n" +
-            "S to move backward\n" +
-            "A to move left\n" +
-            "D to move right\n" +
-            "Ear control: \n" +
-            "2 - forward \n" +
-            "x - back \n" +
-            "f - freeze/unfreeze \n " +
-            "space - reset ear"
-                            );
-        
-    }
+    public GameObject page1;
+    public GameObject page2;
+    public GameObject page3;
+    public GameObject page4;
+    public GameObject page5;
+    public GameObject page6;
+
+
+
+
+    private int pageCounter = 1;
+
+
+    private int pageAmount = 6;
+
+
 
     private void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            pageCounter += pageCounter;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            pageCounter = pageCounter-1;
+        }
+        if (pageCounter < 1)
+        {
+            pageCounter = 1;
+        }
+        if (pageCounter > pageAmount)
+        {
+            pageCounter = pageAmount;
+        }
+        ChangeInstructionIntro();
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (pageCounter == pageAmount)
+            {
+                Scene2();
+            }
+            
+        }
         
     }
 
-    private void ChangeInstruction()
+    private void ChangeInstructionIntro()
     {
+        if (pageCounter == 1)
+        {
+            disvar.text = page1.GetComponent<Text>().text;
+            
+        }
+        if (pageCounter == 2)
+        {
+            disvar.text = page2.GetComponent<Text>().text;
+        }
+
+        if (pageCounter == 3)
+        {
+
+            disvar.text = page3.GetComponent<Text>().text;
+        }
+
+        if (pageCounter == 4)
+        {
+            disvar.text = page4.GetComponent<Text>().text;
+        }
+
+        if (pageCounter == 5)
+        {
+            disvar.text = page5.GetComponent<Text>().text;
+        }
+        if (pageCounter == 6)
+        {
+            disvar.text = page6.GetComponent<Text>().text;
+        }
+
 
     }
+
+    private Func<List<GameObject>> onRequestNumberOfPages;
+    public void SetOnRequestNumberOfPages(Func<List<GameObject>> returnEvent)
+    {
+
+        onRequestNumberOfPages = returnEvent;
+    }
+
+    public List<GameObject> RequestNumberOfPages()
+    {
+        if (onRequestNumberOfPages != null)
+        {
+            return onRequestNumberOfPages();
+        }
+            
+
+        return null;
+    }
+
+    private static void Scene2()
+    {
+        SceneManager.LoadScene("The labirinth");
+    }
 }
+
+
